539843968aec936f4dd95c04dcae72530a86fcec
TGR <nicolafradin@hotmail.com>
2015-11-22 19:04:18 +0100

    add requestAnimationFrame polyfill
    	modified:   index.html
    	modified:   jsClock.js

--- jsClock.js
+++ jsClock.js
@@ -3,7 +3,6 @@
  * studio clock for Web Audio API
  *  
  *  TGrif 2015 - Licence MIT
- *  
  */
 
 
@@ -11,30 +10,26 @@
 
 
 
-            jsClock = function() {
-                
-                this.audioCtx = window.AudioContext();
-                this.clock = document.getElementById('studio_clock');
-                
-                this._initClock();
-                
-                this.time = 0;
-                this.timeElapse = 0;
-//                this.timePause = 0;
+                jsClock = function() {
 
-                this.running = false;
-                
-                
-            };
+                    this.audioCtx = window.AudioContext() || window.webkitAudioContext();
+                    
+                    this.clock = document.getElementById('studio_clock');
+
+                    this._initClock();
+
+                    this.time = 0;
+                    this.timeElapse = 0;
 
 
-//var now = new Date();
-//var audioLoadOffset = (new Date() - now) / 1000;
-//console.log(audioLoadOffset);
+                };
+
+
 
 
 
             jsClock.prototype = {
+                
                 
                 
                 _addZero: function(num) {
@@ -42,22 +37,55 @@
                 },
             
             
+            
                 _initClock: function() {
-                    // TODO reset audioCtx.currentTime
-//                    this.timeElapse = this._getTime();
+                    
+                    this.timePause = 0;
+                    this.isRunning = false;
                     this.clock.textContent = "00:00:00:00";
+                    
                 },
                 
                 
+                
+                _stopAnimation: function() {
+                    
+                    try {
+                        cancelAnimationFrame(this.frame);
+                    } catch (e) { //clearTimeout fallback
+                        clearTimeout(this.frame);
+                    }
+                    
+                },
+                
+                
+                
+                _startAnimation: function() {
+                    
+                        try {
+                            this.frame = requestAnimationFrame(
+                                    jsClock.prototype.displayTime.bind(this)
+                                );
+                        } catch (e) { //setTimeout fallback
+                            this.frame = window.setTimeout(
+                                    jsClock.prototype.displayTime.bind(this)
+                                , 1000 / 60);
+                        }
+
+                },
+                
+                
+                
                 _getTime: function() {
+                    //TODO find fallBack for audioCtx.currentTime
                     return this.audioCtx.currentTime;
                 },
                 
                 
-                _formatTime: function(time) {            
-                    // inspired by https://github.com/jipodine/web-audio-clock/
+                
+                _formatTime: function(time) {
                     
-                        var
+                      var
 
                             milli       = (time % 1).toFixed(2).split('.')[1],
                             seconds     = this._addZero( ~~ (time % 60)),
@@ -71,41 +99,60 @@
                 
                 
                 
-                displayTime: function() {                    
-                //var audioLoadOffset = (new Date() - this.now) / 1000;
-                //console.log(audioLoadOffset);
-                    this.running = true;
-                    this.time = this._getTime() - this.timeElapse/* - audioLoadOffset*/;
+                
+                
+                displayTime: function() {
                     
-                    this.clock.textContent = this._formatTime(this.time);
-
-
-                    this.frame = requestAnimationFrame(
-                            jsClock.prototype.displayTime.bind(this)
-                        );
-          
+                    this.time = this._getTime() - this.timeElapse + this.timePause;
+                    
+                    this.clock.textContent = this._formatTime(this.time);                    
+                    
+                        this._startAnimation();
+                        
                 },
+                
+                
+                
+                startClock: function() {
+                    
+                    this.isRunning = true;
+                    this.timeElapse = this._getTime();
+                    this.displayTime();
+                    
+                },
+                
                 
                 
                 pauseClock: function() {
-                    this.running = false;
-//                    this.timePause = this._getTime();
-//                    this.timeElapse+=this.timePause;
-//                    this.timeElapse = this._getTime();
-                    cancelAnimationFrame(this.frame);
+                    
+                    if (this.isRunning) {
+                        
+                        this.isRunning = false;
+                        this.timePause = this.time;
+
+                            this._stopAnimation();
+                    }
+                    
                 },
                 
                 
-                resetClock: function() {
+                
+                stopClock: function() {
                     
-                    this.running = false;
                     this.timeElapse = this._getTime();
                     this._initClock();
                     
-                    cancelAnimationFrame(this.frame);
+                        this._stopAnimation();
                     
+                },
+                
+                
+                
+                clockIsRunning: function() {
+                    return this.isRunning;
                 }
                 
+
 
             };
         
@@ -113,25 +160,22 @@
 
 
         
+        
         var studioClock = new jsClock();
         
-//        studioClock.displayTime();
         
         
-        
-        document.getElementById('start').onclick = function() {
-            studioClock.displayTime();
-        };
-        
-        document.getElementById('pause').onclick = function() {
-            studioClock.pauseClock();
-        };
-        
-        document.getElementById('stop').onclick = function() {
-            studioClock.resetClock();
-        };
+            document.getElementById('start').onclick = function() {
+                studioClock.startClock();
+            };
+
+            document.getElementById('pause').onclick = function() {
+                studioClock.pauseClock();       
+            };
+
+            document.getElementById('stop').onclick = function() {
+                studioClock.stopClock();
+            };
 
 
 
-//console.log(performance);
-//console.log(performance.now() * 1e-3);
