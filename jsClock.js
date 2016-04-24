aa486c523bb9536c92b792cc41f151f41a012d35
TGrif <thomas.griffault@direct-shoot.com>
2016-04-24 20:53:48 +0200

    Improved formatting
    	modified:   index.html
    	modified:   jsClock.js
    	modified:   style.css

--- jsClock.js
+++ jsClock.js
@@ -1,192 +1,281 @@
 /**
- *  jsClock
- * studio clock for Web Audio API
+ * jsClock
+ *	 Web Audio Studio clock
  *  
  *  TGrif 2015 - Licence MIT
  */
 
 
-    var
+	var
 
-
-
-                jsClock = function() {
-
-                    this.clock = document.getElementById('studio_clock');
-                    
-                    
-                    this.audioCtx = window.AudioContext() || window.webkitAudioContext();
-                    
-                    document.getElementById('sampleRate').innerHTML = this.audioCtx.sampleRate + " Hz";
-
-
-                    this._initClock();
-
-                    this.time = 0;
-                    this.timeElapse = 0;
-
-
-                };
-
-
-
-
-
-            jsClock.prototype = {
-                
-                
-                
-                _addZero: function(num) {
-                    return (num < 10) ? '0' + num : num;
-                },
+		audioContext = window.AudioContext || window.webkitAudioContext,
             
-            
-            
-                _initClock: function() {
+		UTC = Date.now(),
                     
-                    this.timePause = 0;
-                    this.isRunning = false;
-                    this.clock.textContent = "00:00:00:00";
-                    
-                },
-                
-                
-                
-                _startAnimation: function() {
-                    
-                        try {
-                            this.frame = requestAnimationFrame(
-                                    jsClock.prototype.displayTime.bind(this)
-                                );
-                        } catch (e) { //setTimeout fallback
-                            this.frame = window.setTimeout(
-                                    jsClock.prototype.displayTime.bind(this)
-                                , 1000 / 60);
-                        }
+					
 
-                },
-                
-                
-                
-                _stopAnimation: function() {
-                    
-                    try {
-                        cancelAnimationFrame(this.frame);
-                    } catch (e) { //clearTimeout fallback
-                        clearTimeout(this.frame);
-                    }
-                    
-                },
-                
-                
-                
-                _getTime: function() {
-                    //TODO find fallBack for audioCtx.currentTime
-                    return this.audioCtx.currentTime;
-                },
-                
-                
-                
-                _formatTime: function(time) {
-                    
-                      var
+			jsClock = function() {
 
-                            milli       = (time % 1).toFixed(2).split('.')[1],
-                            seconds     = this._addZero( ~~ (time % 60)),
-                            minutes     = this._addZero( ~~ ((time / 60) % 60)),
-                            hours       = this._addZero( ~~ ((time / (60 * 60)) % 60));
+				this.version = '0.0.1';
+
+				this.clock = document.getElementById('studio_clock');
+
+				this.audioCtx = new audioContext();
+
+				this.time = 0;
+				this.timeElapse = 0;
+
+				this.tempo = 120;
+				this.mesure = '4/4';
 
 
-                    return hours + ':' + minutes + ':' + seconds + ':' + milli;
+				this._initClock();
+
+
+				this.metronome = {
+
+					isRunning: false,
+
+					tempo: this.tempo,
+					mesure: this.mesure/*,
+
+					osc: function() { this.audioCtx.createOscillator(); }*/
+					//TODO why this.audioCtx doesn't work in this case ?
+				}
+
+			}
+
+
+
+			jsClock.prototype = {
+
+
+				_addZero: function (num) {
+					return (num < 10) ? '0' + num : num;
+				},            
+
+
+				_initClock: function() {
+
+					this.timePause = 0;
+					this.isRunning = false;
+					this.clock.textContent = "00:00:00:00";
+
+					if (this.audioCtx) {
+
+						this.setTempo();
+						this.setMesure();
+
+						document.getElementById('sampleRate').innerHTML = this.audioCtx.sampleRate + " Hz";
+
+					}
+
+				},                
+
+
+				_startAnimation: function() {
+
+					try {
+						this.frame = requestAnimationFrame(jsClock.prototype._displayTime.bind(this));
+					} catch (e) {
+						console.warn(e);
+						console.info('setTimeout fallback');						
+						this.frame = window.setTimeout(jsClock.prototype._displayTime.bind(this), 1000 / 60);
+					}
+
+				},
+
+
+				_stopAnimation: function() {
+
+					try {
+						cancelAnimationFrame(this.frame);
+					} catch (e) {
+						console.warn(e);
+						console.info('clearTimeout fallback');
+						clearTimeout(this.frame);
+					}
+
+				},                
+
+
+				_getTime: function() {
+					return this.audioCtx.currentTime;
+					//TODO fallBack
+				},
+                
+
+				_formatTime: function (time) {
+
+						var
+
+							milli       = (time % 1).toFixed(2).split('.')[1],
+							seconds     = this._addZero( ~~ (time % 60)),
+							minutes     = this._addZero( ~~ ((time / 60) % 60)),
+							hours       = this._addZero( ~~ ((time / (60 * 60)) % 60));
+
+					return hours + ':' + minutes + ':' + seconds + ':' + milli;
+
+				},
+
+
+				_displayTime: function() {
+
+					this.time = this._getTime() - this.timeElapse + this.timePause;
+					this.clock.textContent = this._formatTime(this.time);
+					this._startAnimation();
+
+				},                
+
+
+
+				startClock: function() {
+
+					this.isRunning = true;
+					this.timeElapse = this._getTime();
+					this._displayTime();
+
+				},                
+
+
+				pauseClock: function() {
+
+					if (this.isRunning) {
+
+						this.isRunning = false;
+						this.timePause = this.time;
+
+						this._stopAnimation();
+
+					}
+
+				},
+
+
+				stopClock: function() {
+
+					this.timeElapse = this._getTime();
+					this._initClock();
+
+					this._stopAnimation();
+
+				},
+
+
+				clockIsRunning: function() {  // deprecated
+					return this.isRunning;
+				},                
+
+
+				clockTime: function() {
+					return this._formatTime(this.time);
+				},
+
+
+				setTempo: function (tempo) {					
+					this.tempo = tempo || this.tempo;  //TODO possibleTempo
+					document.getElementById('bpm').innerHTML = this.tempo;
+				},                
+
+
+				setMesure: function (mesure) {
+
+					var possibleMesure = [ '4/4', '3/4', '5/4' ];
+
+					if (possibleMesure.indexOf(mesure) !== -1) {
+						this.mesure = mesure || this.mesure;
+					}
+
+					document.getElementById('mesure').innerHTML = this.mesure;
+
+				},
+
+
+				setMetronome: function() {
+
+					if (this.metronome.isRunning == false) {
+						this.metronome.isRunning = true;
+						console.info('starting metronome');
+					} else {
+						this.metronome.isRunning = false;
+						console.info('stoping metronome');
+					}
+
+//                    console.log(this.time);
                     
-                },
-                
-                
-                
-                
-                
-                displayTime: function() {
+                    // TODO make it blink
+                    // TODO implements good metronome by cwillo
                     
-                    this.time = this._getTime() - this.timeElapse + this.timePause;
                     
-                    this.clock.textContent = this._formatTime(this.time);                    
-                    
-                        this._startAnimation();
+                    /*if (this.isRunning) {
                         
-                },
-                
-                
-                
-                startClock: function() {
-                    
-                    this.isRunning = true;
-                    this.timeElapse = this._getTime();
-                    this.displayTime();
-                    
-                },
-                
-                
-                
-                pauseClock: function() {
-                    
-                    if (this.isRunning) {
-                        
-                        this.isRunning = false;
-                        this.timePause = this.time;
+                        var img = document.getElementById('metronome');
 
-                            this._stopAnimation();
-                    }
+                        var interval = window.setInterval(function() {
+                            if (img.display == 'hidden') {
+                                img.style.visibility = 'visible';
+                            } else {
+                                img.style.visibility = 'hidden';
+                            }
+                        }, 1000);
+                    }*/
                     
-                },
-                
-                
-                
-                stopClock: function() {
                     
-                    this.timeElapse = this._getTime();
-                    this._initClock();
-                    
-                        this._stopAnimation();
-                    
-                },
-                
-                
-                
-                clockIsRunning: function() {
-                    return this.isRunning;
-                },
-                
-                
-                
-                clockTime: function() {
-                    return this._formatTime(this.time);
                 }
                 
+                
 
-
-            };
+            }
         
 
 
 
         
-        
-        var studioClock = new jsClock();
-        
+    var studioClock = new jsClock();
         
         
-            document.getElementById('start').onclick = function() {
-                studioClock.startClock();
-            };
+		
+//        document.getElementById('start').onclick = () => studioClock.startClock();
+        document.getElementById('start').onclick = function() {
+            studioClock.startClock();
+        }
 
-            document.getElementById('pause').onclick = function() {
-                studioClock.pauseClock();       
-            };
+        document.getElementById('pause').onclick = function() {
+            studioClock.pauseClock();
+        }
 
-            document.getElementById('stop').onclick = function() {
-                studioClock.stopClock();
-            };
+        document.getElementById('stop').onclick = function() {
+            studioClock.stopClock();
+        }
 
 
 
+
+        document.getElementById('bpm').onclick = function() {
+            studioClock.setTempo(
+                    prompt('tempo', this.innerHTML)
+                );
+        }
+
+
+        document.getElementById('mesure').onclick = function() {            
+            studioClock.setMesure(
+                    prompt('mesure', this.innerHTML)
+                );
+        }
+
+
+        document.getElementById('metronome').onclick = function() {
+            studioClock.setMetronome();
+        }
+
+
+
+
+// TODO calculer la durée d'une piste et afficher le temps restant
+
+//TODO keep tempo and mesure in local storage or cookie when refresh page
+
+
+//console.log(studioClock.metronome.osc());
 
